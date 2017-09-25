f2dce7cc10d27146eb411b472a67b6754936f2c7
GabrielPora <gabriel.groener@gmail.com>
2017-09-25 13:59:04 +0200

    adding menu

--- includes/menu.hpp
+++ includes/menu.hpp
@@ -5,10 +5,31 @@
 #ifndef BOMBERMAN_MENU_H
 #define BOMBERMAN_MENU_H
 
+#include "../includes/handle.hpp"
 
 class menu {
 
-};
+public:
+    enum class State {
+        START,
+        END,
+        PAUSE,
+        SETTINGS,
+        SAVE,
+        LOAD,
+        LEVEL
+    };
 
+    menu( void );
+    menu( GLFWwindow *window, int width, int height );
+    ~menu( void );
+
+    menu( menu const & copy );
+    menu& operator=( menu const & copy );
+
+    void    setWinPosition(nanogui::Window *window, Eigen::Vector2i vec);
+    void    setWinLayout(nanogui::Window *window);
+
+};
 
 #endif //BOMBERMAN_MENU_H
